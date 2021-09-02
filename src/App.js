e73de8624859d3410b3381bf35675066166f487c
Ingrid-98 <ingrid.opdebeeck@gmail.com>
2021-09-02 15:22:31 +0300

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/ingrid-opdebeeck-1b1b11148/"
+          target="_blank"
+        >
+          {" "}
+          Ingrid Opdebeeck
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/Ingrid-98/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }

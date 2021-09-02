d0a5b45d4b86c9ffd60cad1feb3e7ca3e1985818
Ingrid-98 <ingrid.opdebeeck@gmail.com>
2021-09-02 15:45:25 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/ingrid-opdebeeck-1b1b11148/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Ingrid Opdebeeck
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/Ingrid-98/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ingrid-opdebeeck-1b1b11148/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Ingrid Opdebeeck
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/Ingrid-98/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

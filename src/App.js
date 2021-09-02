1bfba5f1d6d40ba5fcd86644f6d9aaaffcab4468
Ingrid-98 <ingrid.opdebeeck@gmail.com>
2021-09-02 15:15:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App hehehe</div>;
+  return <div className="App">Weather App </div>;
 }

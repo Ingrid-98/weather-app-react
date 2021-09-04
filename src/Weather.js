4e32e5f7b3137870773a78cb2c2627dcdd6741c7
Ingrid-98 <ingrid.opdebeeck@gmail.com>
2021-09-04 17:50:59 +0300

    APi integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     setWeatherData({
@@ -58,8 +58,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "e32e276f7fbc10da2ad0ce3f431a1ad1";
-    let city = "Tokyo";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }

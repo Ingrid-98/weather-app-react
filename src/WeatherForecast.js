61bfab565383fe65eac7b922fe0cdbc79fd062e0
Ingrid-98 <ingrid.opdebeeck@gmail.com>
2021-09-18 13:13:56 +0300

    added forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,14 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
-export default function WeatherForecast() {
+import axios from "axios";
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+  let apiKey = "e32e276f7fbc10da2ad0ce3f431a1ad1";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
